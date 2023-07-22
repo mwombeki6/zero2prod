40417247f45281b7d9dbb10625453549957d3150
mwombeki <hackerlubex@protonmail.com>
2023-07-22 13:35:40 +0300

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use zero2prod::run;
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    run()?.await
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
