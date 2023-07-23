fc671e948fffbb4f9070a4009cd8bb131d7e1f96
mwombeki <hackerlubex@protonmail.com>
2023-07-23 20:59:12 +0300

    add main.rs in tests

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -8,15 +8,13 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 // The function is asynchronous now!
-async fn spawn_app() -> TestApp {
+pub async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
     let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database)
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = configure_database(&configuration.database).await;
     let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     TestApp {
