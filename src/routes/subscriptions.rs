4283c23da45bf742bc89f3501eedbb2f5524aeae
mwombeki <hackerlubex@protonmail.com>
2023-07-23 20:08:33 +0300

    refactor

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,12 +9,12 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(/* 
+pub async fn subscribe(
     form: web::Form<FormData>,
     // Retrieve a connection from the application state!
-    pool: web::Data<PgPool>,*/
+    pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    match sqlx::query!(/* 
+    match sqlx::query!(
         r#"
         INSERT INTO subscription (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -22,7 +22,7 @@ pub async fn subscribe(/*
         Uuid::new_v4(),
         form.email,
         form.name,
-        Utc::now()*/
+        Utc::now(),
     )
     .execute(pool.get_ref())
     .await
--- tests/helpers.rs
+++ tests/helpers.rs
@@ -1,4 +1,5 @@
 use sqlx::PgPool;
+use sqlx::types::Uuid;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -11,7 +12,8 @@ async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
