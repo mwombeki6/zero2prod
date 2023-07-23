f5f3a1c5689df2718fd245491ab9aa97cf14c2d2
mwombeki <hackerlubex@protonmail.com>
2023-07-23 19:23:35 +0300

    commit

---
+++ src/startup.rs
@@ -0,0 +1,22 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
+use std::net::TcpListener;
+
+pub fn run(
+    listener: TcpListener,
+
+    connection: PgConnection
+) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+
+            .app_data(connection)
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
