93bf84e9b301480494b7619ce34b1a1bf101b017
mwombeki <hackerlubex@protonmail.com>
2023-07-24 21:13:06 +0300

    add configuration file

--- src/configuration.rs
+++ src/configuration.rs
@@ -16,7 +16,7 @@ pub struct DatabaseSettings {
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
 
-    settings.merge(config::File::with_name("config"))?;
+    settings.merge(config::File::with_name("configuration"))?;
 
     settings.try_into()
 }
