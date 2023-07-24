cb6b58ce23942753f28a96fe2f4f2a84b8e62077
mwombeki <hackerlubex@protonmail.com>
2023-07-24 23:28:29 +0300

    add crate in startup.rs

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,8 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -9,7 +11,7 @@ pub fn run(
 
     db_pool: PgPool
 ) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
