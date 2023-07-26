5c50fec72b2bd3b93d1397ad48992f8669b43349
mwombeki <hackerlubex@protonmail.com>
2023-07-26 18:36:57 +0300

    add tracing's in subscription.rs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::types::Uuid;
 use sqlx::PgPool;
-use tracing
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -23,6 +23,9 @@ pub async fn subscribe(
         subscriber_name = %form.name
     );
     let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
+    );
     log::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber.",
         request_id,
@@ -44,6 +47,7 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
@@ -54,6 +58,7 @@ pub async fn subscribe(
             HttpResponse::Ok().finish()
         }
         Err(e) => {
+            tracing::error!("Failed to execute query: {:?}", e);
             log::error!(
                 "request_id {} - Failed to execute query: {:?}",
                 request_id,
