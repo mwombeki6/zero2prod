3d2e90ef091354da185989c4abde2e2cc502aa2a
mwombeki <hackerlubex@protonmail.com>
2023-07-29 00:07:46 +0300

    cleaning up initialisation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-use tracing::{Subscriber, subscriber::set_global_default};
+use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
-pub fn get_subscriber(
-    name: String,
-    env_filter: String
-) -> impl Subscriber + Send + Sync {
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new(env_filter));
-    
-    let formatting_layer = BunyanFormattingLayer::new(
-        name,
-        std::io::stdout()
-    );
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
     Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
