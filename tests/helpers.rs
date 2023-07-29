feb6c151beb970668ea324836fe89df258d56d98
mwombeki <hackerlubex@protonmail.com>
2023-07-29 16:27:23 +0300

    refactor

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -13,9 +13,11 @@ pub async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
+
     let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
+    
     let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     TestApp {
