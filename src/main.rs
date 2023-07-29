b7d3146accb007a4b862dcbf319d8e75ae509894
mwombeki <hackerlubex@protonmail.com>
2023-07-29 16:17:27 +0300

    add telemetry in main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use zero2prod::
+use zero2prod::telemetry::{get_subscriber, init_subscriber}
 
 pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
     let env_filter =
